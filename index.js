d20a14401e31460264a1076015ef4eb54cab1887
IshanPoudel <ishan.poudel@mavs.uta.edu>
2023-01-25 21:11:54 -0600

    Added counter

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-document.getElementById("myButton").onclick = function()
+//counter program. 
+// everytime you click the counter , the point increases. 
+
+count = 0;
+document.getElementById("counter").onclick = function()
 {
-    console.log("I clicked submit");
-    
-
-    a = document.getElementById("base").value ;
-    b = document.getElementById("width").value;
-
-    // Check to see if both buttons are filled.
-    if (!(a&&b))
-    {
-        document.getElementById("p1").innerHTML = "Please fill both the buttons. ";
-        return;
-
-
-    }
-
-    //Convert it to number..
-    a = Number(a);
-    b = Number(b);
-    c = Math.pow(a , 2) + Math.pow(b , 2);
-    console.log(c);
-    c = Math.sqrt(c);
-
-    document.getElementById("p1").innerHTML = "The value of the traingle is " + c;
-
-
-
+    count += 1;
+    document.getElementById("p1").innerHTML = "Count: " + count;
 }
- console.log("I am done with Javascript");
