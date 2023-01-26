85223fc59f3baaf26abef9ac591e56c7d753eacf
IshanPoudel <ishan.poudel@mavs.uta.edu>
2023-01-25 21:45:40 -0600

    Added radio boxes

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-//counter program. 
-// everytime you click the counter , the point increases. 
-
-count = 0;
-document.getElementById("counter").onclick = function()
+document.getElementById("myButton").onclick = function()
 {
-    count += 1;
-    document.getElementById("p1").innerHTML = "Count: " + count;
+    const myCheckBox = document.getElementById("myCheckBox");
+    // if(myCheckBox.checked)
+
+    // {
+    //     document.getElementById("p1").innerHTML = "You are subscribed";
+    // }
+    // else
+    // {
+    //     document.getElementById("p1").innerHTML = "You are not subscribed";
+
+
+    // }
+
+    const visaBtn = document.getElementById("visaBtn");
+    const mastercardBtn = document.getElementById("masterCardBtn");
+    const paypalBtn = document.getElementById("paypalBtn");
+
+
+    if (visaBtn.checked)
+    {
+        document.getElementById("p1").innerHTML = "You picked visa";
+
+    }
+    else if (mastercardBtn.checked)
+    {
+        document.getElementById("p1").innerHTML = "You picked mastercard";
+    }
+    else if (paypalBtn.checked)
+    {
+        document.getElementById("p1").innerHTML = "You picked Paypal";
+    }
+
 }
