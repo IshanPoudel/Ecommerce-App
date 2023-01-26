3ced71238e903d208183a93042e4a152a2f24a57
IshanPoudel <ishan.poudel@mavs.uta.edu>
2023-01-25 21:04:02 -0600

    Added calculator

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-console.log("I like pizza!");
-
-
-
-let age;
-
-age = 21;
-
-let firstName = "Ishan"
-let student = false;
-
-console.log(firstName);
-console.log(age);
-console.log ("You are " , age , "years old");
-console.log(student);
-
-document.getElementById("p1").innerHTML = "Hello "+ firstName;
-
-
-
-let username;
-
-//First you need an onclick listener. 
 document.getElementById("myButton").onclick = function()
 {
     console.log("I clicked submit");
     
-    username = document.getElementById("myText").value;
-    console.log(username);
-    document.getElementById("p1").innerHTML = "Hello "+ username;
-}
 
-let x = Number("3.14");
-let y= String(3.14);
-let z=Boolean("pizza");
+    a = document.getElementById("base").value ;
+    b = document.getElementById("width").value;
+
+    // Check to see if both buttons are filled.
+    if (!(a&&b))
+    {
+        document.getElementById("p1").innerHTML = "Please fill both the buttons. ";
+        return;
+
+
+    }
+
+    //Convert it to number..
+    a = Number(a);
+    b = Number(b);
+    c = Math.pow(a , 2) + Math.pow(b , 2);
+    console.log(c);
+    c = Math.sqrt(c);
+
+    document.getElementById("p1").innerHTML = "The value of the traingle is " + c;
+
+
+
+}
+ console.log("I am done with Javascript");
