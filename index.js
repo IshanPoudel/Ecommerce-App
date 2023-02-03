4bc723dbbdb16029d09e691fc97068d489252b27
IshanPoudel <ishan.poudel@mavs.uta.edu>
2023-02-02 23:46:31 -0600

    Ishan

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
-function fToC(temp)
-{
-    temp = (temp-32)*(5/9);
-    return temp;
+//rest parameters. 
+// Packs arguments into an array
 
+
+let total = sum (2,3);
+
+sum (2 ,3 , displayOnDom)
+
+function sum (x ,y , callBack)
+{
+    result = x+y;
+    callBack(result);
 }
 
-function cToF(temp)
+function displayConsole(output)
 {
-    temp = (temp*1.8)+32;
-    return temp;
+    console.log(output);
 }
 
-function printParagraph(text)
+function displayOnDom(output)
 {
-    document.getElementById("p1").innerText = text;
+    document.getElementById("p1").innerHTML = output + "is the total sum";
 }
-
-document.getElementById("myButton").onclick = function()
-{
-    // see which radio box is checked. 
-    const Celsius = document.getElementById("celsius");
-    const Fahreneit = document.getElementById("fahreneit");
-
-    temp = document.getElementById("myTemp").value; 
-    if (!temp)
-    {
-        printParagraph("Please enter a temperature.")
-        return;
-
-    }
-
-    //check if radiobox is checked. 
-
-    if (Celsius.checked)
-    {
-        temp = Number(temp);
-        newTemp = fToC(temp);
-        printParagraph(newTemp + "Degree Celsius");
-    }
-    else if (Fahreneit.checked)
-    {
-        temp = Number(temp);
-        newTemp = cToF(temp);
-        printParagraph(newTemp + "Degree Fahreneit");
-
-    }
-    else
-    {
-        printParagraph("Please pick a button");
-    }
-}
+//Need to add arrowfunction 
